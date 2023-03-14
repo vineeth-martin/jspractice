37e300ca652f226e514fdea11b5fe89876f8f968
Vineeth Martin <vineethmartinthina@gmail.com>
2023-03-15 01:09:45 +0530

    addeventlisteners

--- drum.js
+++ drum.js
@@ -4,7 +4,92 @@
 var a=["w drum","a drum","s drum","d drum","j drum","k drum","l drum"]
 for(var i=0;i<7;i++){
 document.querySelectorAll('button')[i].addEventListener('click',function(){
-    alert('I got clicked')
+this.style.color = "white";
+var key=this.innerHTML;
+switch (key) {
+    case 'w':
+        var tom1=new Audio('tom-1.mp3');
+        tom1.play();
+
+        break;
+    case 'a':
+        var tom2=new Audio('tom-2.mp3');
+        tom2.play();
+
+        break;
+    case 's':
+        var tom3=new Audio('tom-3.mp3');
+        tom3.play();
+
+        break;
+    case 'd':
+        var tom4=new Audio('tom-4.mp3');
+        tom4.play();
+
+        break;    
+    case 'j':
+        var crash=new Audio('crash.mp3');
+        crash.play();
+
+        break;
+    case 'k':
+        var kickbass=new Audio('kick-bass.mp3');
+        kickbass.play();
+
+        break;
+    case 'l':
+        var snare=new Audio('snare.mp3');
+        snare.play();
+
+        break;
+    default:
+        break;
+}
+
+})
+document.addEventListener('keypress',function($event){
+    var k1=$event.key
+    console.log($event.key)
+    switch (k1) {
+        case 'w':
+            var tom1=new Audio('tom-1.mp3');
+            tom1.play();
+    
+            break;
+        case 'a':
+            var tom2=new Audio('tom-2.mp3');
+            tom2.play();
+    
+            break;
+        case 's':
+            var tom3=new Audio('tom-3.mp3');
+            tom3.play();
+    
+            break;
+        case 'd':
+            var tom4=new Audio('tom-4.mp3');
+            tom4.play();
+    
+            break;    
+        case 'j':
+            var crash=new Audio('crash.mp3');
+            crash.play();
+    
+            break;
+        case 'k':
+            var kickbass=new Audio('kick-bass.mp3');
+            kickbass.play();
+    
+            break;
+        case 'l':
+            var snare=new Audio('snare.mp3');
+            snare.play();
+    
+            break;
+    
+        default:
+            break;
+    }
 })
 }
 
