c94668dea9fcda835c79a08ba38519667b0f4e4c
Vineeth Martin <vineethmartinthina@gmail.com>
2023-03-19 09:05:43 +0530

    working on jquery element selection

--- jquery.js
+++ jquery.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    $("h1").css("color","red")
+    // $("h1").css("color","red")
+    // $("h1").text("please dont click")
+
+    $("h1").addClass("text-property")
+});
+
+console.log($("h1").hasClass("text-property"))
+$("h1").removeClass("text-property")
+
+$("h1").click(function(){
+    $("h1").css("color","green")
+});
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+});
+$("h1").on("click",function(){
+    $("h1").text("Got clicked");
 });
