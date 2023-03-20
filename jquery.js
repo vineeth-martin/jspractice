6b3ce8eda977cd95b24a0d3d23a36bf8baa3a9d1
Vineeth Martin <vineethmartinthina@gmail.com>
2023-03-21 00:57:00 +0530

    commiting

--- jquery.js
+++ jquery.js
@@ -17,3 +17,19 @@ $(document).keypress(function(event){
 $("h1").on("click",function(){
     $("h1").text("Got clicked");
 });
+console.log($("img").attr("src"))
+$("a").attr("src","https://www.yahoo.com")
+for (var i=0;i<6;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        $("h1").css("color","red")
+    });
+}
+$("input").keypress(function(event){
+    console.log(event.key);
+    $("h1").text(event.key)
+})
+$("h1").append("<button type='button' >Button</button>")
+$("h1").before("<button type='button' >Button</button>")
+$("button").click,function(){
+    $("h1").slideUp().slideDown().animate({opacity:0.5})
+}
